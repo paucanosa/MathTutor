4dc18acc55744eb8f52a3746d5c87f7687ad56ec
snofi <l.a.wu@student.tudelft.nl>
2021-10-13 16:27:49 +0200

    three levels of practices, answer with remainder added

--- src/main/kotlin/furhatos/app/spacereceptionist/nlu/nlu.kt
+++ src/main/kotlin/furhatos/app/spacereceptionist/nlu/nlu.kt
@@ -1,7 +1,9 @@
 package furhatos.app.spacereceptionist.nlu
 
+import furhatos.nlu.ComplexEnumEntity
 import furhatos.nlu.Intent
 import furhatos.util.Language
+import furhatos.nlu.common.Number
 
 
 // CheckIn Intent
@@ -18,5 +20,31 @@ class CheckIn : Intent() {
 class Confused : Intent() {
     override fun getExamples(lang: Language): List<String> {
         return listOf("Who are you?", "Where am I?", "What?", "What is this?", "What the hell is this?")
+    }
+}
+
+class DivisionAnswer(var answer : DivisionAnswerInfo? = null) : Intent(){
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "@answer")
+    }
+}
+
+class DivisionAnswerInfo(var quotient : Number = Number(0),
+               var remainder : Number = Number(0)) : ComplexEnumEntity() {
+
+    override fun getEnum(lang: Language): List<String> {
+        return listOf(
+            "The quotient is @quotient.",
+            "@quotient",
+            "It is @quotient",
+            "left with @remainder",
+            "The remainder is @remainder.",
+            "The answer is @quotient.",
+            "remainders @remainder")
+    }
+
+    override fun toText(): String {
+        return generate("$quotient $remainder")
     }
 }
